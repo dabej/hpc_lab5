047ac2f9ca8f81036190fe94e06dde967909c884
david <asdkdas>
2021-10-01 16:15:38 +0200

    working until computation

--- diffusion.c
+++ diffusion.c
@@ -15,7 +15,8 @@ int main (int argc, char *argv[]) {
 	MPI_Comm_size(MPI_COMM_WORLD, &nmb_mpi_proc);
 	MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
 
-	const int width, height;
+	const int width = 100, height = 100;
+	const float d = 0.033;
 	const int bcast_root = 0;
 	float *a;
 
@@ -25,11 +26,8 @@ int main (int argc, char *argv[]) {
 		FILE *fp = fopen("init", "r");
 		fscanf(fp, "%d %d", &width, &height);
 
-		a = (float*) malloc(sizeof(float)*width*height);
+		a =  malloc(sizeof(float)*width*height);
 		
-		for (size_t i = 0; i < width*height; i++)
-			a[i] = 0.;
-
 		int row, col;
 		float temp;
 		while (fscanf(fp, "%d %d %f", &col, &row, &temp) == 3) {
@@ -40,6 +38,8 @@ int main (int argc, char *argv[]) {
 		printf("done parsing file\n");
 	}
 
+	MPI_Barrier(MPI_COMM_WORLD);
+	
 	const int sz = width * height;
 	// With this expression we round the quotient up.
 	const int rows_loc = (height - 1) / nmb_mpi_proc + 1;
@@ -47,27 +47,38 @@ int main (int argc, char *argv[]) {
 	if ( mpi_rank != bcast_root )
 		a = malloc(sz*sizeof(float));
 
+
+	float *dummy = malloc(8);
 	int from, froms[nmb_mpi_proc];
 	int to, tos[nmb_mpi_proc];
 	if ( mpi_rank == bcast_root ) {
+		dummy[0] = 0.;
+		dummy[1] = 1.;
 		for ( int jx = 0, from = 0; jx < nmb_mpi_proc; ++jx, from += width*rows_loc) {
 			froms[jx] = from;
 			tos[jx] = from + width*rows_loc <= sz ? from + width*rows_loc : sz;
 		}
 	}
-
+	
 	// send indices to each node
 	MPI_Scatter(froms, 1, MPI_INT, &from, 1, MPI_INT, bcast_root, MPI_COMM_WORLD);
 	MPI_Scatter(tos, 1, MPI_INT, &to, 1, MPI_INT, bcast_root, MPI_COMM_WORLD);
 
+
+
+	printf("from %d to %d received at: %d\n",from,to,mpi_rank);
+
+	
 	// bcast data array	
 	MPI_Bcast(a, sz, MPI_FLOAT, bcast_root, MPI_COMM_WORLD);
 
-	if ( mpi_rank != bcast_root ){
-		printf("from %d to %d received at: %d\n",from,to,mpi_rank);
-		printf("%f received at %d\n",a[0],mpi_rank);
-	}
 
+		
+
+	printf("%f received at %d\n",a[0],mpi_rank);
+
+/*	
+	float *res = malloc((to - from) * sizeof(float));
 	// Computation here
 	for (size_t i = from; i < to; i++) {
 		float value = a[i];
@@ -86,10 +97,15 @@ int main (int argc, char *argv[]) {
 			down = a[i + width];
 
 		value += d * ((up+down+left+right)/4 - value);
-		c_loc[i-from] = value;
-		printf("%f\n", value);
+		res[i-from] = value;
 	}
 
+	float *merged = NULL;
+	if ( mpi_rank == bcast_root)
+		merged = malloc(sz*sizeof(float));
+	
+	MPI_Gather(res,to-from,MPI_FLOAT,merged,to-from,MPI_FLOAT,bcast_root, MPI_COMM_WORLD);
+*/
 	MPI_Finalize();
 
 	return 0;
